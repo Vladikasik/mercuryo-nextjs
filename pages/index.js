46a742b9e6fc98b230e1b9296e7f022313e8572e
Vladislav Ainshtein <vladislav.ainshtein@gmail.com>
2024-10-04 12:33:17 +0200

    init2

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    // Ensure the Mercuryo widget script runs only after the page has loaded
+    const script = document.createElement('script');
+    script.src = "https://widget.mercuryo.io/embed.2.0.js";
+    script.async = true;
+    script.onload = () => {
+      // Call the Mercuryo widget
+      mercuryoWidget.run({
+        widgetId: '27daec1a-932c-4543-a2b0-2ca4946cb590',
+        host: document.getElementById('mercuryo-widget'),
+        type: 'buy',
+        fiatCurrency: 'EUR',
+        cryptoCurrency: 'SOL',
+        fiatAmount: 100,
+      });
+    };
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      <h1>Getting started</h1>
+      <div id="mercuryo-widget"></div>
+    </div>
+  );
+}
