c71d03a28721f739bb45866f3c11edb701474b17
Vladislav Ainshtein <vladislav.ainshtein@gmail.com>
2024-10-04 13:17:38 +0200

    div for widget

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
 import { useEffect } from 'react';
 
+// export default function Home() {
+//   useEffect(() => {
+//     // Ensure the Mercuryo widget script runs only after the page has loaded
+//     const script = document.createElement('script');
+//     script.src = "https://widget.mercuryo.io/embed.2.0.js";
+//     script.async = true;
+//     script.onload = () => {
+//       // Call the Mercuryo widget
+//       mercuryoWidget.run({
+//         widgetId: '27daec1a-932c-4543-a2b0-2ca4946cb590',
+//         host: document.getElementById('mercuryo-widget'),
+//         type: 'buy',
+//         fiatCurrency: 'EUR',
+//         cryptoCurrency: 'SOL',
+//         fiatAmount: 100,
+//       });
+//     };
+//     document.body.appendChild(script);
+//   }, []);
+
+//   return (
+//       <div id="mercuryo-widget"></div>
+//   );
+// }
+
 export default function Home() {
   useEffect(() => {
-    // Ensure the Mercuryo widget script runs only after the page has loaded
     const script = document.createElement('script');
     script.src = "https://widget.mercuryo.io/embed.2.0.js";
     script.async = true;
     script.onload = () => {
-      // Call the Mercuryo widget
       mercuryoWidget.run({
         widgetId: '27daec1a-932c-4543-a2b0-2ca4946cb590',
         host: document.getElementById('mercuryo-widget'),
@@ -21,6 +44,15 @@ export default function Home() {
   }, []);
 
   return (
-      <div id="mercuryo-widget"></div>
+    <div
+      id="mercuryo-widget"
+      style={{
+        width: '100%',
+        height: '100vh', // Adjust this as needed
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    ></div>
   );
 }
